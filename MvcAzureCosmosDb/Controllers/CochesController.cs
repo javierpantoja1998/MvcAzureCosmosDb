9780e5555e0baa682678ca546b2e1650e1b797d1
javierpantoja1998 <cientificodebanco@gmail.com>
2023-04-26 12:54:48 +0200

    Add project files.

---
+++ MvcAzureCosmosDb/Controllers/CochesController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcAzureCosmosDb.Models;
+using MvcAzureCosmosDb.Services;
+
+namespace MvcAzureCosmosDb.Controllers
+{
+    public class CochesController : Controller
+    {
+        private ServicesCosmosDb service;
+
+        public CochesController(ServicesCosmosDb service)
+        {
+            this.service = service;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string accion)
+        {
+            await this.service.CreateDataBaseAsync();
+            ViewData["MENSAJE"] = "Recursos creados en azure cosmos db";
+            return View();
+        }
+
+        public async Task<IActionResult> Vehiculos()
+        {
+            List<Vehiculo> vehiculos = 
+                await this.service.GetVehiculosAsync();
+            return View(vehiculos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Vehiculo car)
+        {
+            await this.service.InsertVehiculosAsync
+                (car);
+            return RedirectToAction("Vehiculos");
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            Vehiculo car = await this.service.FindVehiculoAsync(id);
+            return View(car);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            Vehiculo car = await this.service.FindVehiculoAsync(id);
+            return View(car);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Vehiculo car)
+        {
+            await this.service.UpdateVehiculoAsync(car);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            await this.service.DeleteVehiculoAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcAzureCosmosDb/Services/ServicesCosmosDb.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Azure.Cosmos;
+using MvcAzureCosmosDb.Models;
+
+namespace MvcAzureCosmosDb.Services
+{
+    public class ServicesCosmosDb
+    {
+        //SE TRABAJA CON ITEMS CONTAINERS
+        //DENTRO DE CONTAINER PODEMOS RECUPERAR EL CONTAINER
+        //LLAMADA COSMOSCLIENT
+        private CosmosClient client;
+        public Container containerCosmos;
+
+        public ServicesCosmosDb(CosmosClient client, Container containerCosmos)
+        {
+            this.client = client;
+            this.containerCosmos = containerCosmos;
+        }
+
+        //TENDREMOS UN METODO PARA CREAR LA BBDD
+        // Y DENTRO UN CONTENEDOR(LA TABLA)
+        public async Task CreateDataBaseAsync()
+        {
+            //HEMOS DICHO QUE LA PK SERA ID
+            //PERO PODEMOS INDICAR DE FORMA EXPLICITA QUE DICHA PK SERA OTRA
+            ContainerProperties properties =
+                new ContainerProperties("containercoches", "/id");
+            //ESTO ES UN ITEMS CONTAINER
+            await this.client.CreateDatabaseIfNotExistsAsync("vehiculoscosmos");
+            //CREAMOS UN NUEVO CONTENEDOR DENTRO DE ITEMS CONTAINER
+            await this.client.GetDatabase("vehiculoscosmos").CreateContainerIfNotExistsAsync(properties);
+
+        }
+
+        public async Task InsertVehiculosAsync(Vehiculo car)
+        {
+            //EN EL MOMENTO DE CREAR UN NUEVO ITEM
+            //DEBEMOS INDICAR EL ITEM Y TAMBIEN SU PARTITION KEY EXPLICITAMENTE
+            await this.containerCosmos.CreateItemAsync<Vehiculo>
+                (car, new PartitionKey(car.Id));
+
+        }
+
+        //METODO PARA RECUPERAR TODOS LOS COCHES
+        public async Task<List<Vehiculo>> GetVehiculosAsync()
+        {
+            //LOS DATOS SE RECUPERAN ATRAVES DE ITERATOR
+            //NECESITAMOS RECORRER LOS ITEMS MIENTRAS EXISTAN
+            var query =
+                this.containerCosmos.GetItemQueryIterator<Vehiculo>();
+            List<Vehiculo> coches = new List<Vehiculo>();
+            while (query.HasMoreResults)
+            {
+                var resultados = await query.ReadNextAsync();
+                //AÑADIMOS LOS COCHES QUE IRA RECUPERANDO A LA COLECCION
+                coches.AddRange(resultados);
+            }
+            return coches;
+        }
+
+        //MODIFICAR
+        public async Task UpdateVehiculoAsync(Vehiculo car)
+        {
+            await this.containerCosmos.UpsertItemAsync<Vehiculo>
+                (car, new PartitionKey(car.Id));
+        }
+
+        public async Task DeleteVehiculoAsync(string id)
+        {
+            await this.containerCosmos.DeleteItemAsync<Vehiculo>
+                (id, new PartitionKey(id));
+        }
+
+        //METODO BUSCAR VEHICULO
+        public async Task<Vehiculo> FindVehiculoAsync(string id)
+        {
+            ItemResponse<Vehiculo> response  =
+                await this.containerCosmos.ReadItemAsync<Vehiculo>
+                (id, new PartitionKey(id));
+            return response.Resource;
+        }
+    }
+}
